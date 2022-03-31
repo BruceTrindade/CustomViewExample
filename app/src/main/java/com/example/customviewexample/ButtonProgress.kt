fc6b908c27802d61cac533ddf53b40d5d09eb953
brucetrindade <brucetrindade2000@gmail.com>
2022-03-31 19:22:19 -0300

    Init project

---
+++ app/src/main/java/com/example/customviewexample/ButtonProgress.kt
@@ -0,0 +1,13 @@
+package com.example.customviewexample
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ButtonProgress @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet,
+    defStyleAttr: Int = 0
+): ConstraintLayout(context, attrs, defStyleAttr) {
+
+}
