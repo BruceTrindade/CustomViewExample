fdf1315859ebf0294a827e73bc6e4ff9fe44a86f
brucetrindade <brucetrindade2000@gmail.com>
2022-04-02 11:43:39 -0300

    Loading done

--- app/src/main/java/com/example/customviewexample/ButtonProgress.kt
+++ app/src/main/java/com/example/customviewexample/ButtonProgress.kt
@@ -3,6 +3,7 @@ package com.example.customviewexample
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.customviewexample.databinding.ProgressButtonBinding
 
@@ -10,31 +11,83 @@ class ButtonProgress @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet,
     defStyleAttr: Int = 0
-): ConstraintLayout(context, attrs, defStyleAttr) {
+) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-        private var title: String? = null
-        private var loadingTitle: String? = null
+    private var title: String? = null
+    private var loadingTitle: String? = null
 
-        private val viewBinding = ProgressButtonBinding
-            .inflate(LayoutInflater.from(context), this, true)
+    private val binding = ProgressButtonBinding
+        .inflate(LayoutInflater.from(context), this, true)
 
-        init {
-            setLayout(attrs)
+    private var state: ProgressButtonState = ProgressButtonState.Normal
+        set(value) {
+            field = value
+            refreshState()
         }
 
-        private fun setLayout( attrs: AttributeSet?){
-            attrs.let {
-                val attributes = context.obtainStyledAttributes(
-                    it,
-                    R.styleable.ButtonProgress
-                )
+    init {
+        setLayout(attrs)
+        refreshState()
+    }
 
-                setBackgroundResource(R.drawable.progress_button)
+    private fun setLayout(attrs: AttributeSet?) {
+        attrs.let {
+            val attributes = context.obtainStyledAttributes(
+                it,
+                R.styleable.ButtonProgress
+            )
 
-                val titleResId = attributes.getResourceId(R.styleable.ButtonProgress_progress_button_title)
+            setBackgroundResource(R.drawable.progress_button)
 
-                attributes.recycle()
+            val titleResId =
+                attributes.getResourceId(R.styleable.ButtonProgress_progress_button_title, 0)
+            if (titleResId != 0) {
+                title = context.getString(titleResId)
             }
+
+            val loadingTitleResId = attributes.getResourceId(
+                R.styleable.ButtonProgress_progress_button_loading_title,
+                0
+            )
+            if (loadingTitleResId != 0) {
+                loadingTitle = context.getString(loadingTitleResId)
+            }
+
+            attributes.recycle()
         }
+    }
+
+
+    private fun refreshState() {
+        isEnabled = state.isEnable
+        isClickable = state.isEnable
+        refreshDrawableState()
+
+        binding.textTitle.run {
+            isEnabled = state.isEnable
+            isClickable = state.isEnable
+
+        }
+
+        binding.progressBar.visibility = state.progressVisibility
+
+        when(state) {
+            ProgressButtonState.Normal -> binding.textTitle.text = title
+            ProgressButtonState.Loading -> binding.textTitle.text = loadingTitle
+        }
+    }
+
+    fun setLoading() {
+        state = ProgressButtonState.Loading
+    }
+
+    fun setNormal() {
+        state = ProgressButtonState.Normal
+    }
+
+    sealed class ProgressButtonState(val isEnable: Boolean, val progressVisibility: Int){
+        object Normal : ProgressButtonState(true, View.GONE)
+        object Loading : ProgressButtonState(false, View.VISIBLE)
+    }
 
 }
